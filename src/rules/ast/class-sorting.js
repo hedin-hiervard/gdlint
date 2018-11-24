db2cd3a7d37dc6d2cc1ff529757b255cb8362302
hedin <flame.beholder@gmail.com>
2018-11-24 13:17:16 +0200

    fix(linting): `class-sorting` rule
    fixed line deducing, added 'export vars' rule

--- src/rules/ast/class-sorting.js
+++ src/rules/ast/class-sorting.js
@@ -3,11 +3,12 @@
 import log from 'log.js'
 
 import type { EmitIssue } from 'engine'
-import type { Node, ClassNode, ClassConstant } from 'ast_types'
+import type { Node, ClassNode, ClassConstant, ClassMember } from 'ast_types'
 
-const minLineReducer = (acc, linedObject: { line: number }) => (isNaN(acc) || linedObject.line < acc) ? linedObject.line : acc
+const minLine = (acc, linedObject: { line: number }) => (isNaN(acc) || linedObject.line < acc) ? linedObject.line : acc
+const maxLine = (acc, linedObject: { line: number }) => (isNaN(acc) || linedObject.line > acc) ? linedObject.line : acc
 
-function isImport(constant: ClassConstant) {
+function isImport(constant: ClassConstant): boolean {
     const expr = constant.expression
     if(expr.type !== 'constant') {
         log.warn(`class constant type is not ConstantNode at ${expr.line}:${expr.col}`)
@@ -19,55 +20,71 @@ function isImport(constant: ClassConstant) {
          expr.value.value.class_name === 'GDScript')
 }
 
+function isExport(member: ClassMember): boolean {
+    return member._export.type !== 'NIL'
+}
+
 const entities = {
-    signals: (cn: ClassNode) => cn._signals
-        .reduce(minLineReducer, NaN),
-    imports: (cn: ClassNode) => cn.constant_expressions
+    signals: (cn: ClassNode, reducer: *) => cn._signals
+        .reduce(reducer, NaN),
+    imports: (cn: ClassNode, reducer: *) => cn.constant_expressions
         .filter(({ value: cn }) => isImport(cn))
         .map(({ value: constant }) => constant.expression)
-        .reduce(minLineReducer, NaN),
-    constants: (cn: ClassNode) => cn.constant_expressions
-        .filter(({ value: cn }) => !isImport(cn))
+        .reduce(reducer, NaN),
+    constants: (cn: ClassNode, reducer: *) => cn.constant_expressions
+        .filter(({ value: cn }) => !isImport(cn) && !cn.is_enum)
         .map(({ value: constant }) => constant.expression)
-        .reduce(minLineReducer, NaN),
-    'vars': (cn: ClassNode) => cn.variables
+        .reduce(reducer, NaN),
+    enums: (cn: ClassNode, reducer: *) => cn.constant_expressions
+        .filter(({ value: cn }) => cn.is_enum)
+        .map(({ value: constant }) => constant.expression)
+        .reduce(reducer, NaN),
+    'exported vars': (cn: ClassNode, reducer: *) => cn.variables
+        .filter(v => isExport(v))
         .filter(v => !v.identifier.startsWith('_'))
-        .reduce(minLineReducer, NaN),
-    'private vars': (cn: ClassNode) => cn.variables
+        .reduce(reducer, NaN),
+    'vars': (cn: ClassNode, reducer: *) => cn.variables
+        .filter(v => !isExport(v))
+        .filter(v => !v.identifier.startsWith('_'))
+        .reduce(reducer, NaN),
+    'private vars': (cn: ClassNode, reducer: *) => cn.variables
+        .filter(v => isExport(v))
         .filter(v => v.identifier.startsWith('_'))
-        .reduce(minLineReducer, NaN),
-    subclasses: (cn: ClassNode) => cn.subclasses
-        .reduce(minLineReducer, NaN),
-    'static private funcs': (cn: ClassNode) => cn.static_functions
+        .reduce(reducer, NaN),
+    subclasses: (cn: ClassNode, reducer: *) => cn.subclasses
+        .reduce(reducer, NaN),
+    'static private funcs': (cn: ClassNode, reducer: *) => cn.static_functions
         .filter(v => v.name.startsWith('_') && v._static)
-        .reduce(minLineReducer, NaN),
-    'static funcs': (cn: ClassNode) => cn.static_functions
+        .reduce(reducer, NaN),
+    'static funcs': (cn: ClassNode, reducer: *) => cn.static_functions
         .filter(v => !v.name.startsWith('_') && v._static)
-        .reduce(minLineReducer, NaN),
-    'private funcs': (cn: ClassNode) => cn.functions
+        .reduce(reducer, NaN),
+    'private funcs': (cn: ClassNode, reducer: *) => cn.functions
         .filter(v => v.name.startsWith('_') && !v._static)
-        .reduce(minLineReducer, NaN),
-    'funcs': (cn: ClassNode) => cn.functions
+        .reduce(reducer, NaN),
+    'funcs': (cn: ClassNode, reducer: *) => cn.functions
         .filter(v => !v.name.startsWith('_') && !v._static)
-        .reduce(minLineReducer, NaN),
+        .reduce(reducer, NaN),
 }
 
 export function apply(node: Node, emitIssue: EmitIssue) {
     if(node.type === 'class') {
         const classNode = node
         const firstLines = {}
+        const lastLines = {}
         for(const entName in entities) {
-            firstLines[entName] = entities[entName](classNode)
+            firstLines[entName] = entities[entName](classNode, minLine)
+            lastLines[entName] = entities[entName](classNode, maxLine)
         }
         for(let idx = 0; idx < Object.keys(entities).length - 1; idx++) {
             const entName = Object.keys(entities)[idx]
             const nextEntName = Object.keys(entities)[idx + 1]
             if(!isNaN(firstLines[nextEntName]) &&
-                !isNaN(firstLines[entName]) &&
-                firstLines[entName] > firstLines[nextEntName]
+                !isNaN(lastLines[entName]) &&
+                lastLines[entName] > firstLines[nextEntName]
             ) {
                 emitIssue({
-                    line: firstLines[entName],
+                    line: firstLines[nextEntName],
                     col: 1,
                     msg: `${entName} should go before ${nextEntName}`,
                 })
